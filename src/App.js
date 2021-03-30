3347000ee60b5d4a4b5cfafe4f7d05a3e61862d2
mcyoo <64882430+mcyoo@users.noreply.github.com>
2021-03-30 22:44:40 +0900

    json data get and click event add

--- src/App.js
+++ src/App.js
@@ -1,13 +1,56 @@
 import React, { Component } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSearch, faAngleUp } from "@fortawesome/free-solid-svg-icons";
+import {
+  faSearch,
+  faAngleUp,
+  faAngleDown,
+} from "@fortawesome/free-solid-svg-icons";
 import SwipeableBottomSheet from "react-swipeable-bottom-sheet";
 import "./assets/main.css";
+import smile_icon from "./assets/img/smile_icon.png";
 
 /*global kakao*/
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      open: false,
+      cafe_list: [],
+      cafe_num: 0,
+      map: null,
+    };
+  }
+
+  openBottomSheet(open) {
+    this.setState({ open });
+  }
+
+  toggleBottomSheet() {
+    this.openBottomSheet(!this.state.open);
+  }
+
+  getJson = () => {
+    let data = fetch("cafe_data.json", {
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    }).then(function (response) {
+      return response.json();
+    });
+    return data;
+  };
+
+  getData = async () => {
+    const { cafe_list } = await this.getJson();
+    this.setState({
+      cafe_list,
+    });
+  };
   componentDidMount() {
+    this.getData();
     const script = document.createElement("script");
     script.async = true;
     script.src =
@@ -16,6 +59,14 @@ class App extends Component {
 
     script.onload = () => {
       kakao.maps.load(() => {
+        function panTo(x, y) {
+          // 이동할 위도 경도 위치를 생성합니다
+          var moveLatLon = new kakao.maps.LatLng(x, y);
+
+          // 지도 중심을 부드럽게 이동시킵니다
+          // 만약 이동할 거리가 지도 화면보다 크면 부드러운 효과 없이 이동합니다
+          map.panTo(moveLatLon);
+        }
         let container = document.getElementById("Mymap");
         let options = {
           center: new kakao.maps.LatLng(33.371776, 126.543786),
@@ -23,65 +74,47 @@ class App extends Component {
         };
         const map = new window.kakao.maps.Map(container, options);
 
-        // 커스텀 오버레이에 표시할 내용입니다
-        // HTML 문자열 또는 Dom Element 입니다
-        var content = '<div class="overlay_info">';
-        content +=
-          '    <a href="https://place.map.kakao.com/m/322286925" target="_blank"><strong>지구별가게</strong></a>';
-        content += '    <div class="desc">';
-        content += '        <img src="jigubul.jpeg" alt="">';
-        content +=
-          '        <span class="address">제주특별자치도 제주시 월랑북2길 16 1층</span>';
-        content += "</div>";
-        content += "</div>";
+        // 마커를 표시할 위치와 title 객체 배열입니다
+        var positions = this.state.cafe_list;
 
-        // 마커를 표시할 위치입니다
-        var position = new kakao.maps.LatLng(33.492789, 126.475179);
+        // 마커 이미지의 이미지 주소입니다
+        var imageSrc = smile_icon;
 
-        // 마커를 생성합니다
-        var marker = new kakao.maps.Marker({
-          position: position,
-          clickable: true, // 마커를 클릭했을 때 지도의 클릭 이벤트가 발생하지 않도록 설정합니다
-        });
+        for (var i = 0; i < positions.length; i++) {
+          // 마커 이미지의 이미지 크기 입니다
+          var imageSize = new kakao.maps.Size(35, 35);
 
-        // 아래 코드는 위의 마커를 생성하는 코드에서 clickable: true 와 같이
-        // 마커를 클릭했을 때 지도의 클릭 이벤트가 발생하지 않도록 설정합니다
-        // marker.setClickable(true);
+          // 마커 이미지를 생성합니다
+          var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize);
 
-        // 마커를 지도에 표시합니다.
-        marker.setMap(map);
+          // 마커를 생성합니다
+          var marker = new kakao.maps.Marker({
+            map: map, // 마커를 표시할 지도
+            position: new kakao.maps.LatLng(
+              positions[i].latlngX,
+              positions[i].latlngY
+            ), // 마커를 표시할 위치
+            title: positions[i].title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
+            image: markerImage, // 마커 이미지
+          });
 
-        // 마커를 클릭했을 때 마커 위에 표시할 인포윈도우를 생성합니다
-        var iwContent = content, // 인포윈도우에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
-          iwRemoveable = true; // removeable 속성을 ture 로 설정하면 인포윈도우를 닫을 수 있는 x버튼이 표시됩니다
-
-        // 인포윈도우를 생성합니다
-        var infowindow = new kakao.maps.InfoWindow({
-          content: iwContent,
-          removable: iwRemoveable,
-        });
-        // 커스텀 오버레이를 생성합니다
-        var mapCustomOverlay = new kakao.maps.CustomOverlay({
-          position: position,
-          content: content,
-          xAnchor: 0.5, // 커스텀 오버레이의 x축 위치입니다. 1에 가까울수록 왼쪽에 위치합니다. 기본값은 0.5 입니다
-          yAnchor: 1.3, // 커스텀 오버레이의 y축 위치입니다. 1에 가까울수록 위쪽에 위치합니다. 기본값은 0.5 입니다
-        });
-
-        // 마커에 클릭이벤트를 등록합니다
-        var click_bool = true;
-        kakao.maps.event.addListener(marker, "click", function () {
-          // 마커 위에 인포윈도우를 표시합니다
-          //infowindow.open(map, marker);
-          // 커스텀 오버레이를 지도에 표시합니다
-          if (click_bool) {
-            mapCustomOverlay.setMap(map);
-            click_bool = false;
-          } else {
-            mapCustomOverlay.setMap(null); // 지도에서 제거한다.
-            click_bool = true;
+          // 마커에 클릭이벤트를 등록합니다
+          kakao.maps.event.addListener(
+            marker,
+            "click",
+            makeOverListener(map, positions[i], this, i)
+          );
+          // 인포윈도우를 표시하는 클로저를 만드는 함수입니다
+          function makeOverListener(map, cafe_data, react_function, num) {
+            return function () {
+              map.setLevel(4);
+              panTo(cafe_data.latlngX - 0.004, cafe_data.latlngY);
+              react_function.setState({ open: true, cafe_num: num });
+              //this.state.open = true;
+              //console.log(this.state.open);
+            };
           }
-        });
+        }
       });
     };
   }
@@ -99,13 +132,28 @@ class App extends Component {
         />
         <div id="Mymap" className="w-screen h-screen z-0"></div>
         <SwipeableBottomSheet
-          overflowHeight={56}
+          overflowHeight={46}
           shadowTip={false}
-          topShadow={true}
+          topShadow={false}
+          open={this.state.open}
+          onChange={this.openBottomSheet.bind(this)}
         >
           <div className="h-99 z-50 flex justify-center">
-            <FontAwesomeIcon icon={faAngleUp} className="mt-2 text-xl" />
+            {this.state.open ? (
+              <FontAwesomeIcon
+                icon={faAngleDown}
+                className="mt-1 text-2xl opacity-50"
+                onClick={this.toggleBottomSheet.bind(this)}
+              />
+            ) : (
+              <FontAwesomeIcon
+                icon={faAngleUp}
+                className="mt-1 text-2xl opacity-50"
+                onClick={this.toggleBottomSheet.bind(this)}
+              />
+            )}
           </div>
+          <div></div>
         </SwipeableBottomSheet>
       </div>
     ); // 이부분이 지도를 띄우게 될 부분.
