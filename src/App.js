9f8463cccfd02c2557ff375664fc4a0df986328f
mcyoo <64882430+mcyoo@users.noreply.github.com>
2021-09-15 14:19:33 +0900

    20210915 cafeMap jejudongne update

--- src/App.js
+++ src/App.js
@@ -174,7 +174,8 @@ class App extends Component {
       "인더그린",
       "버터행성706",
       "죠지트럭커피",
-      "발트글라스"
+      "발트글라스",
+      "제주동네"
     ];
 
     for (var i = 0; i < positions.length; i++) {
@@ -482,27 +483,3 @@ function SamplePrevArrow(props) {
 }
 
 export default App;
-
-/*
- <li>
-                    <a className="block hover:bg-gray-50">
-                      <div className="px-4 py-4 sm:px-6">
-                        <div className="flex items-center justify-between">
-                          <p className="text-sm font-thin text-gray-700 truncate"></p>
-                          <div className="ml-2 flex-shrink-0 flex">
-                            <p className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-yellow-100 text-yellow-800">
-                              마감
-                            </p>
-                          </div>
-                        </div>
-                        <div className="mt-2 sm:flex sm:justify-between">
-                          <div className="sm:flex">
-                            <p className="flex items-center text-lg font-light text-gray-500">
-                              카페스물다섯
-                            </p>
-                          </div>
-                        </div>
-                      </div>
-                    </a>
-                  </li>
-*/
